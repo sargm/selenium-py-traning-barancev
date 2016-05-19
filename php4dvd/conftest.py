f2a43d7680b696a7b7e69c35a57ce6451b679f33
sgm <sgm@htc-cs.ru>
2016-05-19 21:08:08 +0400

    set selenium_fixture in conftest
    for run tests in diffrent files in one brawser

--- php4dvd/conftest.py
+++ php4dvd/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from selenium import webdriver
+from model.application import Application
 
 
 def pytest_addoption(parser):
@@ -6,10 +8,23 @@ def pytest_addoption(parser):
     parser.addoption("--base_url", action="store", default="http://localhost:9080/php4dvd/", help="base URL")
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def browser_type(request):
     return request.config.getoption("--browser")
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def base_url(request):
     return request.config.getoption("--base_url")
+
+
+@pytest.fixture(scope="session")
+def app(request, browser_type, base_url):
+    if browser_type == "firefox":
+        driver = webdriver.Firefox()
+    elif browser_type == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_type == "ie":
+        driver = webdriver.Ie()
+    #driver.implicitly_wait(30)
+    request.addfinalizer(driver.quit)   #close brawser
+    return Application(driver, base_url)
